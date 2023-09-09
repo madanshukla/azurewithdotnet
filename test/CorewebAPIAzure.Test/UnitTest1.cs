db766a0dd0d23f461770e7cedcd1c92b2427ed22
madanshukla <shukla.madan267@gmail.com>
2023-09-09 21:07:46 +0530

    test case changeer

--- test/CorewebAPIAzure.Test/UnitTest1.cs
+++ test/CorewebAPIAzure.Test/UnitTest1.cs
@@ -9,6 +9,6 @@ public class UnitTest1
     {
 ValuesController values=new ValuesController();
 var test=values.Get();
-Assert.Equal(test,new string[] { "value123564" });
+Assert.Equal(test,new string[] { "value1234" });
     }
 }
