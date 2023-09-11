1ad08b519186f85c2c27c15fc1cb63af8877c7e7
madanshukla <shukla.madan267@gmail.com>
2023-09-11 20:08:08 +0530

    test

--- test/CorewebAPIAzure.Test/UnitTest1.cs
+++ test/CorewebAPIAzure.Test/UnitTest1.cs
@@ -7,6 +7,8 @@ public class UnitTest1
     [Fact] //Arrange Act Assert
     public void Test1()
     {
+
+        
 ValuesController values=new ValuesController();
 var test=values.Get();
 Assert.Equal(test,new string[] { "value123" });
